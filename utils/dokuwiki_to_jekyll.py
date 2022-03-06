72131da845c233754b30249f4860b5adde610faa
TAKAHASHI Kyohei <kcrt@kcrt.net>
2022-03-07 00:24:53 +0900

    Show update information

--- utils/dokuwiki_to_jekyll.py
+++ utils/dokuwiki_to_jekyll.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from pathlib import Path
+import shutil
 import subprocess
 import frontmatter  # python-frontmatter
 import datetime
@@ -14,7 +15,7 @@ def parse_args():
     arg_parser = argparse.ArgumentParser(
         description="Convert Dokuwiki to Jekyll")
     arg_parser.add_argument(
-        "-i", "--input", help="dokuwiki's /data/pages", type=str, required=True)
+        "-i", "--input", help="dokuwiki's data folder", type=str, required=True)
     arg_parser.add_argument(
         "-o", "--output", help="jekyll's posts, recommend empty dir", type=str, required=True)
     return arg_parser.parse_args()
@@ -33,15 +34,16 @@ def do_pandoc(dokuwiki):
 def main():
     args = parse_args()
     inputpath = Path(args.input)
-    for file in inputpath.glob("**/*.txt"):
+    for file in (inputpath / "pages").glob("**/*.txt"):
         print(file)
-        relpath = file.relative_to(inputpath)
+        relpath = file.relative_to(inputpath / "pages")
         parts = list(relpath.parts)  # from tuple
         parts[-1] = parts[-1].replace(".txt", "")
         parts = [unquote(part) for part in parts]
         parts.insert(0, "medicine")
 
         markdown = do_pandoc(file.read_text())
+
         post = frontmatter.Post(markdown)
         post["date"] = datetime.datetime.now().strftime(
             "%Y-%m-%d %H:%M:%S +0900")
@@ -56,6 +58,17 @@ def main():
         savepath = Path(args.output).joinpath(*parts)
         savepath.parent.mkdir(parents=True, exist_ok=True)
         savepath.write_text(frontmatter.dumps(post))
+        print(savepath)
+
+    for file in (inputpath / "media").glob("**/*.*"):
+        print(file)
+        relpath = file.relative_to(inputpath / "media")
+        parts = list(relpath.parts)
+        parts = [unquote(part) for part in parts]
+        parts[0] = "medicine"
+        savepath = Path(args.output).joinpath(*parts)
+        savepath.parent.mkdir(parents=True, exist_ok=True)
+        shutil.copy(file, savepath)
 
 
 if __name__ == "__main__":
